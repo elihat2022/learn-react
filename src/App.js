511552c31b1ed46de75049444ff0ef9b699d7a81
elihat2022 <cacereselihat@gmail.com>
2024-08-18 17:25:28 -0500

    Learning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Nav from './components/Nav'
+import Header from './components/Header'
+import Promo from './components/Promo'
+import Intro1 from './components/Intro1.js';
+import Intro2 from './components/Intro2.js';
+import Intro3 from './components/Intro3.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  <div className='App'>
+    <Nav/>
+    
+    <Header name = "Elihat"/>
+    This is my first component
+    <Promo/>
+    <Intro1/>
+    <Intro2/>
+    <Intro3/>
+    <Footer/>
+
+  </div>)
 }
 
 export default App;
